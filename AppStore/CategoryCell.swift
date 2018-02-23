93bf1eb9701783699aa4a7452495532f98ea2f2e
sinbad <sinbad@sinbads-Mac.local>
2018-02-23 11:26:12 -0800

    part done: - collection view

--- AppStore/CategoryCell.swift
+++ AppStore/CategoryCell.swift
@@ -36,9 +36,26 @@ class CategoryCell : UICollectionViewCell, UICollectionViewDelegate, UICollectio
         return collectioView
     }()
     
+    let dividerLine : UIView = {
+       let div = UIView()
+        div.backgroundColor = UIColor.gray
+        div.translatesAutoresizingMaskIntoConstraints = false
+        return div
+    }()
+    
+    let TitleLabel : UILabel = {
+       let title = UILabel()
+        title.text = "Best New App"
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        return title
+    }()
+    
     func setupView(){
         backgroundColor = UIColor.clear
         addSubview(appsCollectionView)
+        addSubview(dividerLine)
+        addSubview(TitleLabel)
         
         appsCollectionView.dataSource = self
         appsCollectionView.delegate = self
@@ -47,9 +64,13 @@ class CategoryCell : UICollectionViewCell, UICollectionViewDelegate, UICollectio
         
         appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: cellId)
         
+         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": TitleLabel]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-14-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": dividerLine]))
+        
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
- 
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[TitleLabel(30)][v0][v1(0.5)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView, "v1": dividerLine, "TitleLabel" : TitleLabel]))
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -66,7 +87,12 @@ class CategoryCell : UICollectionViewCell, UICollectionViewDelegate, UICollectio
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: 100, height: frame.height)
+        return CGSize(width: 100, height: frame.height - 32)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
     }
 }
 
@@ -80,12 +106,51 @@ class AppCell: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-
-    func setupViews(){
     
-        backgroundColor = UIColor.black
+    
+    func setupViews(){
+        
+        addSubview(SetImageView)
+        addSubview(NameLabel)
+        addSubview(CategoryLabel)
+        addSubview(PriceLabel)
+        SetImageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+        NameLabel.frame = CGRect(x: 0, y: frame.width + 2, width: frame.width, height: 40)
+        CategoryLabel.frame = CGRect(x: 0, y: frame.width + 38, width: frame.width, height: 20)
+        PriceLabel.frame = CGRect(x: 0, y: frame.width + 60, width: frame.width, height: 10)
     }
+    
+    let SetImageView : UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "frozen")
+        image.contentMode = .scaleAspectFill
+        image.layer.cornerRadius = 16
+        image.layer.masksToBounds = true
+        return image
+    }()
+    
+    let NameLabel : UILabel = {
+        let name = UILabel()
+        name.text = "Disney Build It: Forzen"
+        name.font = UIFont.systemFont(ofSize: 12)
+        name.numberOfLines = 2
+        return name
+    }()
+    let CategoryLabel : UILabel = {
+        let name = UILabel()
+        name.text = "Entertainment"
+        name.font = UIFont.systemFont(ofSize: 12)
+        return name
+    }()
+    
+    let PriceLabel : UILabel = {
+        let name = UILabel()
+        name.text = "$1.0"
+        name.font = UIFont.systemFont(ofSize: 12)
+        name.textColor = UIColor.darkGray
+        return name
+    }()
+    
 }
 
 
--- AppStore/FeaturedViewController.swift
+++ AppStore/FeaturedViewController.swift
@@ -41,7 +41,7 @@ class FeaturedViewController: UICollectionViewController, UICollectionViewDelega
    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: view.frame.width, height: 150)
+        return CGSize(width: view.frame.width, height: 230)
     }
     
 }
